38640b515b8e22d287707271930344c1f05481f8
GHADEER KHASAWNEH <GHADEER_KHASAWNEH@YAHOO.COM>
2021-05-24 17:06:45 +0300

    editing java question 1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 'use strict'
-let introdcution = prompt('Do you think you know me personally?');
-introduction = introduction.toLowerCase;
+let introduction = prompt('Do you think I study software development?');
+introduction = introduction.toLowerCase();
+if(introduction === 'yes' || introduction === 'y')
+{
+    // console.log('Yes. You are right!')
+    alert('Yes. You are right!')
+} else if (introduction === 'no' || introduction === 'n') {
+alert('Your answer is wrong')
+}else {alert('try one more time')
+
+};
+
+let userName = prompt("Hello! what is your name?")
+alert('Hello ' + userName + ' You are welcomed!')
+alert("I hope you will have fun browsing this website " + userName)
+
