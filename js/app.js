cb3e5fd1e0d2a2fbbab1a6c7377103b8317d462a
GHADEER KHASAWNEH <GHADEER_KHASAWNEH@YAHOO.COM>
2021-05-25 02:02:41 +0300

    editing html file content

--- js/app.js
+++ js/app.js
@@ -1,77 +1,77 @@
-'use strict'
-let introduction = prompt('Do you think I study software development?');
-introduction = introduction.toLowerCase();
-if(introduction === 'yes' || introduction === 'y')
-{
-    // console.log('Yes. You are right!')
-    alert('Yes. You are right!')
-} else if (introduction === 'no' || introduction === 'n'){
-alert('Your answer is wrong')
-}else {
-    alert('try one more time')
+// 'use strict'
+// let introduction = prompt('Do you think I study software development?');
+// introduction = introduction.toLowerCase();
+// if(introduction === 'yes' || introduction === 'y')
+// {
+//     // console.log('Yes. You are right!')
+//     alert('Yes. You are right!')
+// } else if (introduction === 'no' || introduction === 'n'){
+// alert('Your answer is wrong')
+// }else {
+//     alert('try one more time')
 
-};
+// };
 
-let gender = prompt('Do you think I am female?')
-gender = gender.toLowerCase();
-if (gender === 'yes' || gender ==='y'){
-    // console.log('Yes. You are right')
-    alert('Yes. You are right!')
+// let gender = prompt('Do you think I am female?')
+// gender = gender.toLowerCase();
+// if (gender === 'yes' || gender ==='y'){
+//     // console.log('Yes. You are right')
+//     alert('Yes. You are right!')
 
-}else if (gender === 'no' || gender === 'n'){
-    alert('NO. Your answers is wrong')
-}else{
-    alert ('Try one more time')
+// }else if (gender === 'no' || gender === 'n'){
+//     alert('NO. Your answers is wrong')
+// }else{
+//     alert ('Try one more time')
 
-}
+// }
 
-let language = prompt('Do you think I speak Arabic?')
-language = language.toLowerCase();
-if(language === 'yes' || language === 'y'){
-// console.log('Nice guess!')
-alert('Nice guess!')
+// let language = prompt('Do you think I speak Arabic?')
+// language = language.toLowerCase();
+// if(language === 'yes' || language === 'y'){
+// // console.log('Nice guess!')
+// alert('Nice guess!')
 
-}else if (language ==='no' || language ==='no'){
-alert('You might want to have another guess!')
-}else {
-    alert('That is not an expected answer')
-}
-
-
-
-
-let food = prompt('Do you think I like meat?')
-food = food.toLowerCase();
-switch(food){
-    case 'yes':
-        // console.log('Actually, I do not like meat')
-        alert('Actually, I do not like meat');
-    break;
-    case 'no':
-            alert('You are right!');
-    break;
-    default :
-            alert('You might have made a typo!');
-
-}
-let website = prompt('Do you like this website?')
-food = website.toLowerCase();
-switch(website){
-    case 'yes':
-        // console.log('Glad to know that')
-        alert('Glad to know that');
-    break;
-    case 'no':
-            alert('We will record your feedback');
-    break;
-    default :
-            alert('You might have made a typo!');
-
-}
+// }else if (language ==='no' || language ==='no'){
+// alert('You might want to have another guess!')
+// }else {
+//     alert('That is not an expected answer')
+// }
 
 
 
 
-let userName = prompt("Hello! what is your name?")
-alert('Hello ' + userName + ' You are welcomed!')
-alert("I hope you will have fun browsing this website " + userName)
+// let food = prompt('Do you think I like meat?')
+// food = food.toLowerCase();
+// switch(food){
+//     case 'yes':
+//         // console.log('Actually, I do not like meat')
+//         alert('Actually, I do not like meat');
+//     break;
+//     case 'no':
+//             alert('You are right!');
+//     break;
+//     default :
+//             alert('You might have made a typo!');
+
+// }
+// let website = prompt('Do you like this website?')
+// food = website.toLowerCase();
+// switch(website){
+//     case 'yes':
+//         // console.log('Glad to know that')
+//         alert('Glad to know that');
+//     break;
+//     case 'no':
+//             alert('We will record your feedback');
+//     break;
+//     default :
+//             alert('You might have made a typo!');
+
+// }
+
+
+
+
+// let userName = prompt("Hello! what is your name?")
+// alert('Hello ' + userName + ' You are welcomed!')
+// alert("I hope you will have fun browsing this website " + userName)
