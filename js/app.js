094d3205057928d7c1521b2fc2fe2e720fd8275f
GHADEER KHASAWNEH <GHADEER_KHASAWNEH@YAHOO.COM>
2021-05-24 17:21:16 +0300

    adding question 2 in js

--- js/app.js
+++ js/app.js
@@ -5,11 +5,24 @@ if(introduction === 'yes' || introduction === 'y')
 {
     // console.log('Yes. You are right!')
     alert('Yes. You are right!')
-} else if (introduction === 'no' || introduction === 'n') {
+} else if (introduction === 'no' || introduction === 'n'){
 alert('Your answer is wrong')
-}else {alert('try one more time')
+}else {
+    alert('try one more time')
 
 };
+
+let gender = prompt('Do you think I am female?')
+gender = gender.toLowerCase();
+if (gender === 'yes' || gender ==='y'){
+    console.log('Yes. You are right')
+    alert('Yes. You are right!')
+
+}else if (gender === 'no' || gender === 'n'){
+    alert('NO. Your answers is wrong')
+}else{
+    alert ('Try one more time')
+}
 
 let userName = prompt("Hello! what is your name?")
 alert('Hello ' + userName + ' You are welcomed!')
